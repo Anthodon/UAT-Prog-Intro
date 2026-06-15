b6a78a8fb288e1cebecebfc6de9d70898f0c6026
Anthodon <donatosanthony@gmail.com>
2026-06-14 19:49:44 -0700

    5.2 Palindrome Checker
    Adjusted strings.js to include palindrome check loops.
    Adjusted variable names throughout.

--- strings.js
+++ strings.js
@@ -3,21 +3,61 @@ function validatepii()
     /*output message*/
     let output = document.getElementById("output");
 
-    /*clear input*/
+    /*clears output message*/
     output.innerHTML = " ";
 
     /*text inputs*/
-    let fName = document.getElementById("fName").value;
-    let lName = document.getElementById("lName").value;
+    let namef = document.getElementById("namef").value;
+    let namel = document.getElementById("namel").value;
     let age = document.getElementById("age").value;
     let ssn = document.getElementById("ssn").value;
     let zip = document.getElementById("zip").value;
 
+    /*marks only the text forms for palindrome check*/
+    let palinput = [
+        /*allows validation message for form fields*/
+        document.getElementById("namef"),
+        document.getElementById("namel")
+    ];
+
+
+    /*runs check for each palinput field*/
+    for (let i = 0; i < palinput.length; i++){
+        /*removes validiation errors before assessment*/
+        palinput[i].setCustomValidity("");
+
+        /*checks text of current field, removes spaces, converts all to lowercase to homogenize characyers*/
+        let cleanstring = palinput[i].value.replaceAll(" ", "").toLowerCase();
+        /*text assumed to contain palindrome until verified otherwise*/
+        let palincheck = true;
+
+        /*character checker, halved due to first and last letter checking*/
+        for (let j = 0; j < cleanstring.length / 2; j++){
+            /*compares first and last letters for match, then proceeds inward*/
+            if (cleanstring[j] != cleanstring[cleanstring.length - 1 - j]){
+                /*if letters do not match, palincheck is unflagged*/
+                palincheck = false;
+                /*because any instance of non matching letters passes the palindrome check, loop is broken upon deflagging*/
+                break;
+            }
+        }
+        /*checks palincheck results, ignores technical violation of single letter names being palindromes*/
+        if (cleanstring.length > 1 && palincheck == true){
+            /*validation error sent if palindrome found*/
+            palinput[i].setCustomValidity("Palindrome found. Please try again.");
+
+            /*sends validation message to the first field with a palindrome*/
+            palinput[i].reportValidity();
+            return false;
+        }
+    }
+
+
     /*combine first and last name*/
-    let fullName = fName + " " + lName;
+    let namefull = namef + " " + namel;
 
    /*check name length*/
-    if (fullName.length > 20)
+    if (namefull.length > 20)
     {
         output.innerHTML += "Name too long! Max 20 characters.<br>";
         return false;
@@ -39,11 +79,10 @@ function validatepii()
     }   
 
     /*secret message displayed after successful submission*/
-    output.innerHTML =
+    output.innerHTML +=
     "Thank you for your compliance! <br>" +
     "We will put this to good use."
 
-/*hard check againt true submission*/
-return false;
-    
+    /*hard check againt true submission*/
+    return false;
 }
